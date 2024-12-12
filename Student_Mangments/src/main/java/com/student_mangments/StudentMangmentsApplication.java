e29e0a31bffbb100989209b5d179f023f1c10fc3
Sonu Jha <143614977+Sonujha12345@users.noreply.github.com>
2024-12-12 08:06:33 +0530

    Add files via upload
    first commit

---
+++ Student_Mangments/src/main/java/com/student_mangments/StudentMangmentsApplication.java
@@ -0,0 +1,13 @@
+package com.student_mangments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentMangmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentMangmentsApplication.class, args);
+	}
+
+}
